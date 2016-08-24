18deaf333c0886a610e0a8582723eece13acc7b6
Fucxy Huang <innocencei.m@gmail.com>
2016-08-24 20:57:02 +0000

    Sim modify

--- sim/run.sh
+++ sim/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 echo "Welcome  5000"
-python3  calc.py 5000 > 5000.out
+python3  calc.py  5000.out
 echo "Welcome  5500"
-python3  calc.py 5500 > 5500.out
+python3  calc.py  5500.out
 echo "Welcome  6000"
-python3  calc.py 6000 > 6000.out
+python3  calc.py  6000.out
 echo "Welcome  6500"
-python3  calc.py 6500 > 6500.out
+python3  calc.py  6500.out
 echo "Welcome  7000"
-python3  calc.py 7000 > 7000.out
+python3  calc.py  7000.out
 echo "Welcome  7500"
-python3  calc.py 7500 > 7500.out
+python3  calc.py  7500.out
 echo "Welcome  8000"
-python3  calc.py 8000 > 8000.out
+python3  calc.py  8000.out
 echo "Welcome  8500"
-python3  calc.py 8500 > 8500.out
+python3  calc.py  8500.out
 echo "Welcome  9000"
-python3  calc.py 9000 > 9000.out
+python3  calc.py  9000.out
 echo "Welcome  9500"
-python3  calc.py 9500 > 9500.out
+python3  calc.py  9500.out
 echo "Welcome 10000"
-python3 calc.py 10000 > 10000.out
+python3 calc.py  10000.out
 echo "end"
